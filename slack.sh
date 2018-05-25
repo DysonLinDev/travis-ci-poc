9e5753e9a5905be34c2137b176caa64fe7307c78
Dyson <dyson.lin@snapask.com>
2018-05-25 20:02:05 +0800

    add slack webhook

---
+++ slack.sh
@@ -0,0 +1,4 @@
+curl -X POST 
+       -H 'Content-type: application/json' \
+       --data '{"text":"This is a line of text.\nAnd this is another one."}' \
+       https://hooks.slack.com/services/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
