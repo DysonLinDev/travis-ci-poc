94bb15a516c627929ff8f19ab8098a3f6a0e95ac
Dyson <dyson.lin@snapask.com>
2018-05-25 20:08:31 +0800

    fix slack webhook

--- slack.sh
+++ slack.sh
@@ -1,4 +1,3 @@
-curl -X POST 
-       -H 'Content-type: application/json' \
-       --data '{"text":"This is a line of text.\nAnd this is another one."}' \
-       https://hooks.slack.com/services/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
+curl -X POST \
+--data-urlencode 'payload={"text":"This is a line of text.\nAnd this is another one."}' \
+https://hooks.slack.com/services/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
